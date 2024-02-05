ed3d87b625332cba167a4ed8d1b1e56eab2c7476
Chahhat <chahhatp@gmail.com>
2024-02-04 20:51:21 -0800

    Lab2 Commit

---
+++ M2-Lab2/js/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",()=>{  
+    window.document.getElementById("empForm").addEventListener("submit", (event)=> {  
+        event.preventDefault();
+        window.console.log("Employee ID:" + window.document.getElementById("id").value);     
+        window.console.log("Name:" + window.document.getElementById("name").value);
+        window.console.log("Extension:" + window.document.getElementById("ext").value);
+        window.console.log("Email:" + window.document.getElementById("email").value);
+        window.console.log("Department:" + window.document.getElementById("department").value);     
+    });
+    
+  });
